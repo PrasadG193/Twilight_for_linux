202231fd166c06f36d301e9c439e5a59fb5c0921
Prasad Ghangal <prasad.ghangal@gmail.com>
2015-07-01 00:45:06 +0530

    color temperature adjustment done

--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,30 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QVector>
+#include <QProcess>
+#include <QDebug>
+#include <QTextCodec>
+#include <QFile>
+#include <QTextStream>
 
 namespace Ui {
 class MainWindow;
 }
+
+
+class rgbs
+{
+    public:
+        double r,g,b;
+        rgbs()
+        {}
+        rgbs(double x,double y,double z)
+        {
+          r=x;g=y;b=z;
+        }
+};
+
 
 class MainWindow : public QMainWindow
 {
@@ -14,12 +34,20 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    QVector<rgbs> v1;
+
 
 private slots:
     void on_bcontroller_actionTriggered(int action);
 
+    void on_tcontroller_actionTriggered(int action);
+
+    void on_btnReset_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
+
+
 
 #endif // MAINWINDOW_H
